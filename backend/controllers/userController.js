24ca197926fd332212fc6fda14250b95d35bac9d
= <nelsonsamvl@gmail.com>
2024-11-07 10:12:32 +0530

    create user with jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModel.js";
+import byCrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, password, email } = req.body;
@@ -11,19 +13,24 @@ const createUser = asyncHandler(async (req, res) => {
   const userExists = await User.findOne({ email });
   if (userExists) return res.status(400).send("User already exists");
 
+  const salt = await byCrypt.genSalt(10);
+  const hashedPassword = await byCrypt.hash(password, salt);
+
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
 
   try {
     await newUser.save();
+    generateToken(res, newUser._id);
+
     res.status(201).json({
       id: newUser._id,
       username: newUser.username,
       email: newUser.email,
-      password: newUser.password,
+      isAdmin: newUser.isAdmin,
     });
   } catch (err) {
     res.status(400).json({ message: "Invalid User data" });
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JSON_SECRET, {
+    expiresIn: "30d",
+  });
+
+  const thirtyDays = 30 * 24 * 60 * 60 * 1000;
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: thirtyDays,
+  });
+
+  return token;
+};
+
+export default generateToken;
